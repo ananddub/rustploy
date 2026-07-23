710f32121e41077ebaee887ae61c7ffaf32d59f7
addynoven <dmcbaditya@gmail.com>
2026-07-23 22:00:39 +0530

    perf: enable route-level lazy loading via generouted/lazy + fix tsconfig for TS 5.9

--- web/rustploy/src/main.tsx
+++ web/rustploy/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Routes } from '@generouted/react-router';
+import { Routes } from '@generouted/react-router/lazy';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
